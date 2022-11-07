d064277a1d0ddf689dbd8fbb0734060be1619e50
keegan <keegan.lee704@gmail.com>
2022-11-07 21:17:06 +0800

    add listen all nodes

---
+++ src/eth-list-node.ts
@@ -0,0 +1,18 @@
+import { ChildAdded } from "../generated/templates/EthListNode/EthListNode";
+import { Node } from "../generated/schema";
+import { EthListNode as NodeTemplate } from "../generated/templates";
+
+export function handleChildAdded(event: ChildAdded): void {
+  let node = new Node(event.params.child.toHex());
+  node.isLeaf = false;
+  node.parent = event.params.parent.toHex();
+  node.name = event.params.name;
+  node.tag = event.params.childrenTag;
+  node.childrenTag = event.params.grandchildrenTag;
+  node.index = event.params.id;
+  node.createdAtBlockNumber = event.block.number;
+  node.createdAtTimestamp = event.block.timestamp;
+  node.save();
+
+  NodeTemplate.create(event.params.child);
+}
